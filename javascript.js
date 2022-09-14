1ecff62cc55275f92f12d2652c8117676ea82416
nsagstrom <n.sagstrom@gmail.com>
2022-09-14 10:10:14 +0200

    qq

--- javascript.js
+++ javascript.js
@@ -1,6 +1,3 @@
-
-
-
 // let grasklippning = document.querySelector('.grasklippning:checked').value;
 // let planteraBlommor = document.querySelector('.planteraBlommor:checked').value;
 // let vattna = document.querySelector('.vattna:checked').value;
@@ -19,198 +16,153 @@
 
 // }
 (function () {
+  var doc = document.documentElement;
+  var w = window;
 
-    var doc = document.documentElement;
-    var w = window;
+  var prevScroll = w.scrollY || doc.scrollTop;
+  var curScroll;
+  var direction = 0;
+  var prevDirection = 0;
 
-    var prevScroll = w.scrollY || doc.scrollTop;
-    var curScroll;
-    var direction = 0;
-    var prevDirection = 0;
+  var header = document.getElementById("header");
 
-    var header = document.getElementById('header')
+  var checkScroll = function () {
+    /*
+     ** Find the direction of scroll
+     ** 0 - initial, 1 - up, 2 - down
+     */
 
-    var checkScroll = function () {
+    curScroll = w.scrollY || doc.scrollTop;
+    if (curScroll > prevScroll) {
+      //scrolled up
+      direction = 2;
+    } else if (curScroll < prevScroll) {
+      //scrolled down
+      direction = 1;
+    }
 
-        /*
-        ** Find the direction of scroll
-        ** 0 - initial, 1 - up, 2 - down
-        */
+    if (direction !== prevDirection) {
+      toggleHeader(direction, curScroll);
+    }
 
-        curScroll = w.scrollY || doc.scrollTop;
-        if (curScroll > prevScroll) {
-            //scrolled up
-            direction = 2;
-        }
-        else if (curScroll < prevScroll) {
-            //scrolled down
-            direction = 1;
-        }
+    prevScroll = curScroll;
+  };
 
-        if (direction !== prevDirection) {
-            toggleHeader(direction, curScroll);
-        }
+  var toggleHeader = function (direction, curScroll) {
+    if (direction === 2 && curScroll > 300) {
+      header.classList.add("hide");
+      prevDirection = direction;
+    } else if (direction === 1) {
+      header.classList.remove("hide");
+      prevDirection = direction;
+    }
+  };
 
-        prevScroll = curScroll;
-    };
-
-    var toggleHeader = function (direction, curScroll) {
-        if (direction === 2 && curScroll > 300) {
-
-
-            header.classList.add('hide');
-            prevDirection = direction;
-        }
-        else if (direction === 1) {
-            header.classList.remove('hide');
-            prevDirection = direction;
-        }
-    };
-
-    window.addEventListener('scroll', checkScroll);
-
+  window.addEventListener("scroll", checkScroll);
 })();
 
 function smoothScrollIntresse() {
-    document.querySelector('.valjTjansterContainer').scrollIntoView({
-        behavior: 'smooth'
-    });
+  document.querySelector(".valjTjansterContainer").scrollIntoView({
+    behavior: "smooth",
+  });
 }
 function smoothScrollBeskrivning() {
-    document.querySelector('.presentation').scrollIntoView({
-        behavior: 'smooth'
-    });
+  document.querySelector(".presentation").scrollIntoView({
+    behavior: "smooth",
+  });
 }
 function smoothScrollTjanster() {
-    document.querySelector('.tjanster-container').scrollIntoView({
-        behavior: 'smooth'
-    });
+  document.querySelector(".tjanster-container").scrollIntoView({
+    behavior: "smooth",
+  });
 }
 
-
-
-const btn = document.querySelector('#btn');
-btn.addEventListener('click', (event) => {
-    let checkboxes = document.querySelectorAll('input[name="tjanst"]:checked');
-    let values = [];
-    checkboxes.forEach((checkbox) => {
-        values.push(checkbox.value);
-    })
-    document.getElementById('valdaTjanster').innerHTML = "Du vill ha hjälp med att " + values + "!";
-    // values.forEach((value) => {
-    //     alert("Du vill ha hjälp med att " + value)
-    // })
-
-
-})
-
-
-
-
-
-
-
+const btn = document.querySelector("#btn");
+btn.addEventListener("click", (event) => {
+  let checkboxes = document.querySelectorAll('input[name="tjanst"]:checked');
+  let values = [];
+  checkboxes.forEach((checkbox) => {
+    values.push(checkbox.value);
+  });
+  document.getElementById("valdaTjanster").innerHTML =
+    "Du vill ha hjälp med att " + values + "!";
+  // values.forEach((value) => {
+  //     alert("Du vill ha hjälp med att " + value)
+  // })
+});
 
 function myFunction() {
-    var rec = document.getElementById("rec");
-    var rec2 = document.getElementById("rec2");
+  var rec = document.getElementById("rec");
+  var rec2 = document.getElementById("rec2");
 
-    rec.style.display = (
-        rec.style.display == "none" ? "block" : "none");
-    rec.style.animation = "mymove 1s infinite";
-    rec2.style.display = (
-        rec2.style.display == "none" ? "block" : "none");
-
+  rec.style.display = rec.style.display == "none" ? "block" : "none";
+  rec.style.animation = "mymove 1s infinite";
+  rec2.style.display = rec2.style.display == "none" ? "block" : "none";
 }
 
-
-
-
 function revealer() {
-    var rev = document.querySelectorAll(".revealRight");
-    for (var x = 0; x < rev.length; i++) {
-        var winHei = window.innerHeight;
-        var eleTop = rev[x].getBoundingClientRect().top;
-        var eleVis = 150;
-        if (eleTop < winHei - eleVis) {
-            rev[x].classList.add("active");
-        }
-        else {
-            rev[x].classList.remove("active");
-        }
+  var rev = document.querySelectorAll(".revealRight");
+  for (var i = 0; x < rev.length; i++) {
+    var winHei = window.innerHeight;
+    var eleTop = rev[x].getBoundingClientRect().top;
+    var eleVis = 150;
+    if (eleTop < winHei - eleVis) {
+      rev[x].classList.add("active");
+    } else {
+      rev[x].classList.remove("active");
     }
+  }
 }
 window.addEventListener("scroll", revealer);
 revealer;
 
-
-
-
-
-
 function reveal() {
-
-    var reveals = document.querySelectorAll(".revealLeft");
-    for (var i = 0; i < reveals.length; i++) {
-        var windowHeight = window.innerHeight;
-        var elementTop = reveals[i].getBoundingClientRect().top;
-        var elementVisible = 150;
-        if (elementTop < windowHeight - elementVisible) {
-            reveals[i].classList.add("active");
-        }
-        else {
-            reveals[i].classList.remove("active");
-        }
-
+  var reveals = document.querySelectorAll(".revealLeft");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
     }
+  }
 }
-
-
-
 
 window.addEventListener("scroll", reveal);
 reveal;
 
-
-
-
-
-
 var x = 1;
 
 function updateClass() {
-    let a = $(".rec.active");
-    a.removeClass("active");
-    a = a.parent().next(".progress");
-    if (a.length == 0)
-        a = $(".progress").first();
-    a.find(".rec").addClass("active");
+  let a = $(".rec.active");
+  a.removeClass("active");
+  a = a.parent().next(".progress");
+  if (a.length == 0) a = $(".progress").first();
+  a.find(".rec").addClass("active");
 }
 
 $(document).ready(function () {
-    setInterval(function () {
-        updateClass();
-    }, x * 5000);
+  setInterval(function () {
+    updateClass();
+  }, x * 5000);
 });
-
-
 
 var o = 1;
 
-
 function updateClass2() {
-    let y = $(".rec2.active2");
-    y.removeClass("active2");
-    y = y.parent().next(".progress2");
-    if (y.length == 0)
-        y = $(".progress2").first();
-    y.find(".rec2").addClass("active2");
+  let y = $(".rec2.active2");
+  y.removeClass("active2");
+  y = y.parent().next(".progress2");
+  if (y.length == 0) y = $(".progress2").first();
+  y.find(".rec2").addClass("active2");
 }
 
 $(document).ready(function () {
-    setInterval(function () {
-        updateClass2();
-    }, o * 5000);
+  setInterval(function () {
+    updateClass2();
+  }, o * 5000);
 });
 
 // ----------bildspel-----------
@@ -219,21 +171,24 @@ let bildIndex = 0;
 bildspel();
 
 function bildspel() {
-    document.querySelectorAll(".enSlide").forEach(item => {
-        if(item.style.display == "none"){
-            
-        }
-    })
-
-
-
-
+  document.querySelectorAll(".enSlide").forEach((item) => {
+    if (item.style.display == "none") {
+    }
+  });
 }
 
 // ------Arvids presentation-----
 
 function slide() {
-    setTimeout(function () {
-        document.getElementById('profilbildPre').style.left = '500px';
-    }, 1000)
+  setTimeout(function () {
+    document.getElementById("profilbildPre").style.left = "500px";
+  }, 1000);
 }
+
+const cart = () => {
+  console.log("wwwww");
+
+  let newWin = window.open("about:blank", "hello", "width=200,height=200");
+
+  newWin.document.write("Hello, world!");
+};
